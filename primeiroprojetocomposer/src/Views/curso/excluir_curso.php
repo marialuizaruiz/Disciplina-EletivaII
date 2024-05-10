2b351f261e85071f0310c16e6e887fc95b16b704
marialuizaruiz <102814134+marialuizaruiz@users.noreply.github.com>
2024-05-10 19:27:38 -0300

    Add files via upload

---
+++ primeiroprojetocomposer/src/Views/curso/excluir_curso.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Excluir Curso</title>
+  </head>
+  <body>
+    <main class="container">
+        <h3>Excluir Curso</h3>
+        <form action="/curso/deletar" method="post">
+            <input type="hidden" name="id" value="<?= $resultado["id"] ?>">
+            <div class="row">
+                <div class="col-6">
+                    <label for="nome" class="form-label">Nome:</label>
+                    <input type="text" disabled name="nome" 
+                        class="form-control" value="<?= $resultado['nome'] ?>">
+                </div>
+                <div class="col-6">
+                    <label for="tempo_duracao" class="form-label">Tempo de Duração:</label>
+                    <input type="text" disabled name="tempo_duracao" 
+                        class="form-control" value="<?= $resultado['tempo_duracao'] ?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>Você deseja realmente excluir esse registro?</p>
+                    <button type="submit" class="btn btn-danger">
+                        Excluir
+                    </button>
+                </div>
+            </div>
+        </form>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
