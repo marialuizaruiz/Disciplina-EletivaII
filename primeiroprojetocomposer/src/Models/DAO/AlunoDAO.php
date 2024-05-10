38fc949fabe43f537706e2b2baa3dbe45e615643
marialuizaruiz <102814134+marialuizaruiz@users.noreply.github.com>
2024-05-10 19:25:02 -0300

    Add files via upload

--- primeiroprojetocomposer/src/Models/DAO/AlunoDAO.php
+++ primeiroprojetocomposer/src/Models/DAO/AlunoDAO.php
@@ -24,4 +24,50 @@ class AlunoDAO{
         }
     }
 
+    public function alterar(Aluno $aluno){
+        try{
+            $sql = "UPDATE aluno SET nome = :nome, curso = :curso
+                    WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":nome", $aluno->getNome());
+            $p->bindValue(":curso", $aluno->getCurso());
+            $p->bindValue(":id", $aluno->getId());
+            return $p->execute();
+        }catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM aluno WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            return $p->execute();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultarTodos(){
+        try{
+            $sql = "SELECT * FROM aluno";
+            return $this->conexao->getConexao()->query($sql);
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
+    public function consultar($id){
+        try{
+            $sql = "SELECT * FROM aluno WHERE id = :id";
+            $p = $this->conexao->getConexao()->prepare($sql);
+            $p->bindValue(":id", $id);
+            $p->execute();
+            return $p->fetch();
+        } catch(\Exception $e){
+            return 0;
+        }
+    }
+
 }
