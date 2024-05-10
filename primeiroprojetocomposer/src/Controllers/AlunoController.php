4f27d6f72cc52427c9b4e1ec055d54807acf2ff0
marialuizaruiz <102814134+marialuizaruiz@users.noreply.github.com>
2024-05-10 19:24:23 -0300

    Add files via upload

--- primeiroprojetocomposer/src/Controllers/AlunoController.php
+++ primeiroprojetocomposer/src/Controllers/AlunoController.php
@@ -7,17 +7,70 @@ use Php\Primeiroprojeto\Models\Domain\Aluno;
 
 class AlunoController{
 
+    public function index($params){
+        $alunoDAO = new AlunoDAO();
+        $resultado = $alunoDAO->consultarTodos();
+        $acao = $params[1] ?? "";
+        $status = $params[2] ?? "";
+        if($acao == "inserir" && $status == "true")
+            $mensagem = "Registro inserido com sucesso!";
+        elseif($acao == "inserir" && $status == "false")
+            $mensagem = "Erro ao inserir!";
+        elseif($acao == "alterar" && $status == "true")
+            $mensagem = "Registro alterado com sucesso!";
+        elseif($acao == "alterar" && $status == "false")
+            $mensagem = "Erro ao alterar!";
+        elseif($acao == "excluir" && $status == "true")
+            $mensagem = "Registro excluído com sucesso!";
+        elseif($acao == "excluir" && $status == "false")
+            $mensagem = "Erro ao excluir!";
+        else 
+            $mensagem = "";
+        require_once("../src/Views/aluno/aluno.php");
+    }
+    
     public function inserir($params){
-        require_once("../src/Views/aluno/inserir_aluno.html"); 
+        require_once("../src/Views/aluno/inserir_aluno.html");
     }
 
     public function novo($params){
         $aluno = new Aluno(0, $_POST['nome'], $_POST['curso']);
         $alunoDAO = new AlunoDAO();
         if ($alunoDAO->inserir($aluno)){
-            return "Inserido com sucesso!";
+            header("location: /aluno/inserir/true");
         } else {
-            return "Erro ao inserir!";
+            header("location: /aluno/inserir/false");
+        }
+    }
+
+    public function alterar($params){
+        $alunoDAO = new AlunoDAO();
+        $resultado = $alunoDAO->consultar($params[1]);
+        require_once("../src/Views/aluno/alterar_aluno.php");
+    }
+
+    public function excluir($params){
+        $alunoDAO = new AlunoDAO();
+        $resultado = $alunoDAO->consultar($params[1]);
+        require_once("../src/Views/aluno/excluir_aluno.php");
+    }
+
+    public function editar($params){
+        $aluno = new Aluno($_POST['id'], $_POST['nome'], $_POST['curso']);
+        $alunoDAO = new AlunoDAO();
+        if ($alunoDAO->alterar($aluno)) {
+            header("location: /aluno/alterar/true");
+        } else {
+            header("location: /aluno/alterar/false");
+        }
+    }
+
+    public function deletar($params){
+        $alunoDAO = new AlunoDAO();
+        if ($alunoDAO->excluir($_POST['id'])){
+            header("location: /aluno/excluir/true");
+        } else {
+            header("location: /aluno/excluir/false");
         }
     }
 
--- primeiroprojetocomposer/src/Controllers/CursoController.php
+++ primeiroprojetocomposer/src/Controllers/CursoController.php
@@ -7,6 +7,28 @@ use Php\Primeiroprojeto\Models\Domain\Curso;
 
 class CursoController{
 
+    public function index($params){
+        $cursoDAO = new CursoDAO();
+        $resultado = $cursoDAO->consultarTodos();
+        $acao = $params[1] ?? "";
+        $status = $params[2] ?? "";
+        if($acao == "inserir" && $status == "true")
+            $mensagem = "Registro inserido com sucesso!";
+        elseif($acao == "inserir" && $status == "false")
+            $mensagem = "Erro ao inserir!";
+        elseif($acao == "alterar" && $status == "true")
+            $mensagem = "Registro alterado com sucesso!";
+        elseif($acao == "alterar" && $status == "false")
+            $mensagem = "Erro ao alterar!";
+        elseif($acao == "excluir" && $status == "true")
+            $mensagem = "Registro excluído com sucesso!";
+        elseif($acao == "excluir" && $status == "false")
+            $mensagem = "Erro ao excluir!";
+        else 
+            $mensagem = "";
+        require_once("../src/Views/curso/curso.php");
+    }
+
     public function inserir($params){
         require_once("../src/Views/curso/inserir_curso.html");
     }
@@ -15,9 +37,40 @@ class CursoController{
         $curso = new Curso(0, $_POST['nome'], $_POST['tempo_duracao']);
         $cursoDAO = new CursoDAO();
         if ($cursoDAO->inserir($curso)){
-            return "Inserido com sucesso!";
+            header("location: /curso/inserir/true");
         } else {
-            return "Erro ao inserir!";
+            header("location: /curso/inserir/false");
+        }
+    }
+
+    public function alterar($params){
+        $cursoDAO = new CursoDAO();
+        $resultado = $cursoDAO->consultar($params[1]);
+        require_once("../src/Views/curso/alterar_curso.php");
+    }
+
+    public function excluir($params){
+        $cursoDAO = new CursoDAO();
+        $resultado = $cursoDAO->consultar($params[1]);
+        require_once("../src/Views/curso/excluir_curso.php");
+    }
+
+    public function editar($params){
+        $curso = new Curso($_POST['id'], $_POST['nome'], $_POST['tempo_duracao']);
+        $cursoDAO = new CursoDAO();
+        if ($cursoDAO->alterar($curso)) {
+            header("location: /curso/alterar/true");
+        } else {
+            header("location: /curso/alterar/false");
+        }
+    }
+
+    public function deletar($params){
+        $cursoDAO = new CursoDAO();
+        if ($cursoDAO->excluir($_POST['id'])){
+            header("location: /curso/excluir/true");
+        } else {
+            header("location: /curso/excluir/false");
         }
     }
 
